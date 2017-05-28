2f49e3cfbf902e05f3cbc693571eb0064e943252
Sinoptic55 <ivtlabs@narod.ru>
2017-05-28 22:48:01 +0300

    Do parsing Json data

--- app/src/main/java/pro/umaks/mrkaznaandroidapp/models/RequestData.java
+++ app/src/main/java/pro/umaks/mrkaznaandroidapp/models/RequestData.java
@@ -2,13 +2,18 @@ package pro.umaks.mrkaznaandroidapp.models;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 /**
  * Created by ivtla on 25.05.2017.
  */
 
 public class RequestData {
     @SerializedName("Requests")
-    public ArrayList<RequestModel> Requests;
+    public ArrayList<RequestModel> Requests = new ArrayList<RequestModel>();
+    public static final Map<String, RequestModel> ITEM_MAP = new HashMap<String, RequestModel>();
     public RequestData() {
     }
 
